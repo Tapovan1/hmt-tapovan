00138b6da40d1c3c297232a50cd82e48f561673c
Patel Swapnil <info.tapovan2@gmail.com>
2025-06-15 17:53:16 +0530

    fix issue

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,6 +1,6 @@
 import { login } from "@/lib/action/user.action";
 import LoginForm from "./Form";
-
+import Image from "next/image";
 
 export default function LoginPage() {
   async function loginAction(prevState: unknown, formData: FormData) {
@@ -23,19 +23,19 @@ export default function LoginPage() {
       <div className="hidden lg:flex lg:w-1/2 bg-[#e6eef8] flex-col items-center justify-center p-8">
         <div className="max-w-md text-center">
           <h1 className="text-4xl font-bold text-[#3b82f6] mb-4">
-            dummy 
+            Tapovan School
           </h1>
           <div className="relative w-full h-45 my-6">
-            {/* <Image
+            <Image
               src="/logo.png"
               alt="Teacher Attendance Portal"
               fill
               className="object-contain"
               priority
-            /> */}
+            />
           </div>
           <h2 className="text-2xl font-semibold text-gray-700 mb-2">
-            Attendance Portal
+            Teacher Attendance Portal
           </h2>
           <p className="text-gray-600">
             Track attendance, manage leaves, and view history all in one place.
@@ -60,9 +60,9 @@ export default function LoginPage() {
             {/* Mobile only school name */}
             <div className="lg:hidden text-center mb-6">
               <h2 className="text-xl font-semibold text-[#3b82f6]">
-               
+                Tapovan School
               </h2>
-              <p className="text-sm text-gray-600">Attendance Portal</p>
+              <p className="text-sm text-gray-600">Teacher Attendance Portal</p>
             </div>
 
             <LoginForm loginAction={loginAction} />
